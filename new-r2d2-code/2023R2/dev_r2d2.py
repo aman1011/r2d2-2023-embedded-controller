e939c8509a69d8f807a2a6f014555d64e8e42698
Aman Gaur <aman.gaur1011@gmail.com>
2024-03-08 19:20:57 -0400

    updating

--- new-r2d2-code/2023R2/dev_r2d2.py
+++ new-r2d2-code/2023R2/dev_r2d2.py
@@ -22,10 +22,10 @@ from pyrf24 import RF24, RF24_PA_LOW
 import sys
 
 #List of selected sounds
-hums = ["Jedi/hum/HUM1.mp3", "Jedi/hum/HUM7.mp3", "Jedi/hum/HUM13.mp3", "Jedi/hum/HUM17.mp3","Jedi/hum/HUM23.mp3"]
-screams = ["Jedi/scream/SCREAM1.mp3", "Jedi/scream/SCREAM2.mp3", "Jedi/scream/SCREAM3.mp3", "Jedi/scream/SCREAM4.mp3"]
-sents = ["Jedi/sent/SENT2.mp3", "Jedi/sent/SENT4.mp3", "Jedi/sent/SENT5.mp3", "Jedi/sent/SENT17.mp3", "Jedi/sent/SENT20.mp3"]
-procs = ["Jedi/proc/PROC2.mp3", "Jedi/proc/PROC3.mp3", "Jedi/proc/PROC5.mp3", "Jedi/proc/PROC13.mp3", "Jedi/proc/PROC15.mp3"]
+hums = ["/home/pi/Desktop/r2d2-2023-embedded-controller/new-r2d2-code/2023R2/Jedi/hum/HUM1.mp3", "/home/pi/Desktop/r2d2-2023-embedded-controller/new-r2d2-code/2023R2/Jedi/hum/HUM7.mp3", "/home/pi/Desktop/r2d2-2023-embedded-controller/new-r2d2-code/2023R2/Jedi/hum/HUM13.mp3", "/home/pi/Desktop/r2d2-2023-embedded-controller/new-r2d2-code/2023R2/Jedi/hum/HUM17.mp3","/home/pi/Desktop/r2d2-2023-embedded-controller/new-r2d2-code/2023R2/Jedi/hum/HUM23.mp3"]
+screams = ["/home/pi/Desktop/r2d2-2023-embedded-controller/new-r2d2-code/2023R2/Jedi/scream/SCREAM1.mp3", "/home/pi/Desktop/r2d2-2023-embedded-controller/new-r2d2-code/2023R2/Jedi/scream/SCREAM2.mp3", "/home/pi/Desktop/r2d2-2023-embedded-controller/new-r2d2-code/2023R2/Jedi/scream/SCREAM3.mp3", "/home/pi/Desktop/r2d2-2023-embedded-controller/new-r2d2-code/2023R2/Jedi/scream/SCREAM4.mp3"]
+sents = ["/home/pi/Desktop/r2d2-2023-embedded-controller/new-r2d2-code/2023R2/Jedi/sent/SENT2.mp3", "/home/pi/Desktop/r2d2-2023-embedded-controller/new-r2d2-code/2023R2/Jedi/sent/SENT4.mp3", "/home/pi/Desktop/r2d2-2023-embedded-controller/new-r2d2-code/2023R2/Jedi/sent/SENT5.mp3", "/home/pi/Desktop/r2d2-2023-embedded-controller/new-r2d2-code/2023R2/Jedi/sent/SENT17.mp3", "/home/pi/Desktop/r2d2-2023-embedded-controller/new-r2d2-code/2023R2/Jedi/sent/SENT20.mp3"]
+procs = ["/home/pi/Desktop/r2d2-2023-embedded-controller/new-r2d2-code/2023R2/Jedi/proc/PROC2.mp3", "/home/pi/Desktop/r2d2-2023-embedded-controller/new-r2d2-code/2023R2/Jedi/proc/PROC3.mp3", "/home/pi/Desktop/r2d2-2023-embedded-controller/new-r2d2-code/2023R2/Jedi/proc/PROC5.mp3", "/home/pi/Desktop/r2d2-2023-embedded-controller/new-r2d2-code/2023R2/Jedi/proc/PROC13.mp3", "/home/pi/Desktop/r2d2-2023-embedded-controller/new-r2d2-code/2023R2/Jedi/proc/PROC15.mp3"]
 
 # Initialize the pygame window. Key inputs will be picked up by
 # it and get translated into commands.
@@ -36,14 +36,29 @@ screen = pygame.display.set_mode((400,400))
 # Set up the joystick
 pygame.joystick.init()
 joystick_count = pygame.joystick.get_count()
+joystick = None
+if not joystick_count:
+	print("No controller found. Waiting for controller to be connected ...")
+	joystick_connected = False
+		
+	while not joystick_connected:
+		for event in pygame.event.get():
+			if event.type == pygame.JOYDEVICEADDED:
+				time.sleep(2)
+				# This event will be generated when the program starts for every
+				# joystick, filling up the list without needing to create them manually.
+				joy = pygame.joystick.Joystick(event.device_index)
+				joystick = joy
+				joystick_connected = True
+				print("Connected the controller ....")
+				print(f"Event device index {event.device_index} ....")
+				print(f"Joystick {joystick} ....")
+				
+else:
+	print("Looks like the controller was connected")
+	joystick = pygame.joystick.Joystick(0)
+	print(f"Joystick {joystick} ....")
 
-
-if joystick_count == 0:
-    print("No joystick found. Exiting...")
-    pygame.quit()
-    sys.exit()
-
-joystick = pygame.joystick.Joystick(0)
 joystick.init()
 
 # Set the serial port and baud rate based on your configuration
@@ -61,7 +76,7 @@ try:
 
     motors.SetSpeed1(240)
     motors.SetSpeed2Turn(240)
-    time.sleep(12)
+    time.sleep(2)
     motors.SetSpeed1(128)
     motors.SetSpeed2Turn(128)
         #motors.SetSpeed2Turn(128)
@@ -73,13 +88,15 @@ except Exception as ex:
 
 
 # Test if the motors work, giving them a little jolt.
-#md49_set_speed(speed1, speed2)
-    # Sabretooth at ttyAMA1.
-    #saber = Sabertooth("/dev/ttyAMA1", timeout=0.1, baudrate=9600, address=128)
-    #saber.drive(1,0)
-#    print("Connected to MD49 motor controller and Sabretooth controller")
-#except Exception as ex:
-#    print(f"Could not connect to MD49 {ex}")
+try:
+    #md49_set_speed(speed1, speed2)
+    # Sabretooth at ttyAMA2.
+    saber = Sabertooth("/dev/ttyAMA3", timeout=0.1, baudrate=9600, address=128)
+    saber.drive(1,0)
+    print("Connected to Sabretooth controller")
+except Exception as ex:
+    print(f"Could not connect to sabretooth {ex}")
+    sys.exit(0)
 
 # Attempt to establish a link with the head via bluetooth.
 #arduinoHead = RF24(22, 0)
@@ -96,6 +113,9 @@ I2C_NUM_COLS = 16
 
 lcd = I2cLcd(1, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
 # Testing LCD
+lcd.clear()
+lcd.putstr("Start R2D2")
+time.sleep(2)
 lcd.clear()
 lcd.putstr("R2D2 NSCC!")
 lcd.move_to(0,1)
@@ -130,7 +150,7 @@ time.sleep(1)
 # openFlap2 = True
 # openFlap3 = True
 # openFlap4 = True
-joystick = 0
+#joystick = 0
 
 forwardValue = 128
 turnValue = 0
@@ -142,7 +162,9 @@ try:
     # This is where we take key inputs and make the robot do things.
     while True:
         for event in pygame.event.get():
+			#print(f"joystick {joystick}")
             print(f"Event is {event} .... ")
+            print(f"joystick is {joystick} ....")
             #joystick hotplug handling
             if event.type == pygame.JOYDEVICEADDED:
                 # This event will be generated when the program starts for every
@@ -260,13 +282,13 @@ try:
                         clearLCDLine()
                         lcd.putstr("SOUND: CANTINA")
                         pygame.mixer.init()
-                        pygame.mixer.music.load("Jedi/mix/CANTINA.mp3")
+                        pygame.mixer.music.load("/home/pi/Desktop/r2d2-2023-embedded-controller/new-r2d2-code/2023R2/Jedi/mix/CANTINA.mp3")
                         pygame.mixer.music.play()
                     
                     elif event.button == 10:
                         print("PS button | ANNOYED")
                         pygame.mixer.init()
-                        pygame.mixer.music.load("Jedi/mix/ANNOYED.mp3")
+                        pygame.mixer.music.load("/home/pi/Desktop/r2d2-2023-embedded-controller/new-r2d2-code/2023R2/Jedi/mix/ANNOYED.mp3")
                         pygame.mixer.music.play()
                         
                     elif event.button == 9:
@@ -274,7 +296,7 @@ try:
                         clearLCDLine()
                         lcd.putstr("SOUND: SH.CIRC")
                         pygame.mixer.init()
-                        pygame.mixer.music.load("Jedi/mix/SHORTCKT.mp3")
+                        pygame.mixer.music.load("/home/pi/Desktop/r2d2-2023-embedded-controller/new-r2d2-code/2023R2/Jedi/mix/SHORTCKT.mp3")
                         pygame.mixer.music.play()
                 elif event.type == pygame.JOYAXISMOTION:
                     
@@ -315,11 +337,11 @@ try:
                         rightMotorValue = 1
                         
                     #send commands to motors
-                    print(f" Right motor value being set {rightMotorValue} ....")
-                    print(f"Left motor value being set {leftMotorValue} ....")
+                    #print(f" Right motor value being set {rightMotorValue} ....")
+                    #print(f"Left motor value being set {leftMotorValue} ....")
                     motors.SetSpeed2Turn(int(rightMotorValue))
                     motors.SetSpeed1(int(leftMotorValue))
-                    #saber.drive(1, int(headValue))
+                    saber.drive(1, int(headValue))
 
                 
                     
@@ -333,16 +355,17 @@ try:
                 lcd.move_to(0,0)
                 lcd.putstr("R2D2 offline!")
                 quit()
-except:
-    #saber.stop()
+except Exception as ex:
+    saber.stop()
     motors.SetSpeed1(128)
     motors.SetSpeed2Turn(128)
     lcd.clear()
     lcd.move_to(0,0)
     lcd.putstr("R2D2 offline!")
     lcd.move_to(0,1)
-    lcd.putstr("Fatal crash")
-pygame.quit()
-#saber.drive(1, 0)
+    lcd.putstr(f"{ex}")
+    print(f"Fatal crash error {ex} ....")
+    pygame.quit()
+    saber.drive(1, 0)
 quit()
 
